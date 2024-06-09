1d5b90018d048f4c999c958ce66d3cd028412919
Warrior Deere <warriorcatjohndeere@protonmail.com>
2024-06-09 15:57:56 +0200

    worked on dispatch menu
    commit towards #11

--- src/app/shared/components/content_module.tsx
+++ src/app/shared/components/content_module.tsx
@@ -1,7 +1,68 @@
+'use client'
+
+import { TbArrowsExchange } from "react-icons/tb";
+import { FaThList } from "react-icons/fa";
+import { BsFillGrid3X3GapFill } from "react-icons/bs";
+
+import { getDB } from "@/app/indexed_db";
+import { MissionInterface, DatabaseOptions } from "../types/types";
+
+import { useEffect, useState } from "react";
+
+import '../style/content_module.css'
+
 export function DispatchContentModule() {
+
+    const [missionData, setMissionData] = useState<MissionInterface[]>([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const dbopt_mission: DatabaseOptions = {
+                database: 'DB_SAVEGAME_DATA',
+                store: 'DB_STORE_ACTIVE_MISSIONS',
+                schema: 'SCHEMA_SAVEGAME_DATA'
+            }
+
+            await getDB(dbopt_mission)
+                .then((r) => {
+                    setMissionData(r as MissionInterface[]);
+                    return r;
+                })
+                .catch((err) => {
+                    console.error(err)
+                });
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <div className="content-module dispatch-menu">
-            <p>Active Dispatches</p>
+            <div className="menu-head-wrapper">
+                <div className="menu-title">
+                    <h2>Einsatzliste</h2>
+                </div>
+                <div className="menu-ui">
+                    <button>
+                        <BsFillGrid3X3GapFill />
+                    </button>
+                    <button>
+                        <FaThList />
+                    </button>
+                    <button>
+                        <TbArrowsExchange />
+                    </button>
+                </div>
+            </div>
+            <div className="menu-content">
+                {
+                    missionData.map((dispatch: MissionInterface) => {
+                        return (
+                            <DispatchContentItem data={dispatch} />
+                        );
+                    })
+                }
+            </div>
         </div>
     )
 }
@@ -12,4 +73,34 @@ export function UnitContentModule() {
             <p>Units</p>
         </div>
     )
+}
+
+function DispatchContentItem({ data }: { data: MissionInterface }) {
+    return (
+        <div className="dispatch-item">
+            <div className="dispatch-icon">
+                icon
+            </div>
+            <h3 className="dispatch-title">
+                {data.mission.type}
+            </h3>
+            <p className="dispatch-detail">
+                {data.mission.specific}
+            </p>
+            <div className="dispatch-units">
+                <div className="unit-tag">
+                    <p>FL-GR 11/11/1</p>
+                </div>
+                <div className="unit-tag">
+                    <p>FL-GR 11/49/1</p>
+                </div>
+                <div className="unit-tag">
+                    <p>FL-GR 11/24/1</p>
+                </div>
+                <div className="unit-tag">
+                    <p>FL-GR 11/33/1</p>
+                </div>
+            </div>
+        </div>
+    );
 }
