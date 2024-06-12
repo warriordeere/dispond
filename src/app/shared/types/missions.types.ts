84402d6c361e28508725af77a7c73479c676e4ac
Warrior Deere <warriorcatjohndeere@protonmail.com>
2024-06-12 20:48:33 +0200

    improvements towards #39
    working on #39

---
+++ src/app/shared/types/missions.types.ts
@@ -0,0 +1,10 @@
+export enum DispatchTypeOptions {
+    "DISPATCH_TYPE_FIRE_A" = 'fire_a',
+    "DISPATCH_TYPE_FIRE_B" = 'fire_b',
+    "DISPATCH_TYPE_FIRE_C" = 'fire_c',
+    "DISPATCH_TYPE_FIRE_D" = 'fire_d',
+    "DISPATCH_TYPE_FIRE_E" = 'fire_e',
+    "DISPATCH_TYPE_TECHNICAL_A" = 'tech_a',
+    "DISPATCH_TYPE_TECHNICAL_B" = 'tech_b',
+    "DISPATCH_TYPE_TECHNICAL_C" = 'tech_c',
+}
